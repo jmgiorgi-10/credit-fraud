58ee43764c889111d7d7aae3f2e4d722736a3f4f
Joaquin Giorgi <jmgiorgi@Joaquins-Air.fibertel.com.ar>
2025-01-17 13:17:29 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,150 @@
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+from tqdm import tqdm
+import pandas as pd
+import matplotlib.pyplot as plt
+from autoencoder import MLP
+from sklearn.model_selection import train_test_split
+
+
+batch_size = 64
+learning_rate = 0.001
+num_epochs = 25
+
+# It contains only numerical input variables which are the result of a PCA transformation. 
+# Unfortunately, due to confidentiality issues, we cannot provide the original features and more background information about the data. 
+# Features V1, V2, … V28 are the principal components obtained with PCA, the only features which have not been transformed with PCA are 'Time' and 'Amount'. 
+# Feature 'Time' contains the seconds elapsed between each transaction and the first transaction in the dataset. 
+# The feature 'Amount' is the transaction Amount, this feature can be used for example-dependant cost-sensitive learning. 
+# Feature 'Class' is the response variable and it takes value 1 in case of fraud and 0 otherwise. 
+
+# 1. Multi-layer perceptron
+# 2. Variational autoencoder
+
+# class for loading a .csv dataset
+class MyDataset(Dataset):
+
+    def __init__(self, data, labels):
+        self.data = data
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        sample = self.data[idx]
+        label = self.labels[idx]
+
+        return sample, label
+    
+def train_one_epoch(train_dataloader, device, optimizer, model, loss):
+
+    epoch_loss = 0
+    for batch_idx, (inputs, targets) in enumerate(tqdm(train_dataloader)):
+
+        inputs = inputs.to(device)
+        targets = targets.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(inputs)
+
+        input_loss = loss(outputs, targets)
+        input_loss.backward()
+        optimizer.step()
+
+        epoch_loss += input_loss.item()
+
+    print(f"Epoch Loss: {epoch_loss:.4f}")
+
+
+def evaluate(test_dataloader, model, device):
+
+    import pdb; pdb.set_trace()
+
+    model.eval()
+    total_loss = 0
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for inputs, targets in tqdm(test_dataloader):
+
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = model(inputs)
+
+            _, predicted_labels = torch.max(outputs, 1)
+            correct += (predicted_labels == targets).sum().item()
+            total += targets.size(0)
+
+        avg_loss = total_loss / total
+        accuracy = correct / total
+
+    return {'loss': avg_loss, 'accuracy': accuracy}
+
+
+
+def main():
+
+    mlp = MLP()
+    device = torch.device('cpu')
+    model = mlp.to(device)
+
+    credit_df = pd.read_csv("creditcard.csv")
+    data_df = credit_df.drop(columns=['Time','Amount','Class'])
+    class_df = credit_df['Class']
+
+    import pdb; pdb.set_trace()
+
+    X_train, X_test, y_train, y_test = train_test_split(data_df, class_df, test_size=0.3, random_state=42)
+    X_train = torch.tensor(X_train.values, dtype=torch.float32)
+    X_test = torch.tensor(X_test.values, dtype=torch.float32)
+    y_train = torch.tensor(y_train.values, dtype=torch.long)
+    y_test = torch.tensor(y_test.values, dtype=torch.long)
+
+    # data = torch.tensor(credit_df.values)
+    # labels = torch.tensor(credit_df['Class'])
+
+    train_dataset = MyDataset(X_train, y_train)
+    test_dataset = MyDataset(X_test, y_test)
+
+    train_dataloader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+    test_dataloader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+    loss = nn.CrossEntropyLoss()
+
+    # set model to training mode, and select desired optimizer #
+    mlp.train()
+    epoch_loss = 0
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001) 
+    # training loop #
+
+    for epoch in range(num_epochs):
+        train_one_epoch(train_dataloader, device, optimizer, model, loss)
+
+    # Evaluation
+    result_dict = evaluate(test_dataloader, model, device)
+    print(f"accuracy: {result_dict['accuracy']}")
+
+    # for batch_idx, (inputs, targets) in enumerate(tqdm(train_dataloader)):
+
+    #     inputs = inputs.to(device)
+    #     targets = targets.to(device)
+
+    #     optimizer.zero_grad()
+    #     outputs = model(inputs)
+
+    #     input_loss = loss(outputs, targets)
+    #     input_loss.backward()
+    #     optimizer.step()
+
+    #     epoch_loss += input_loss.item()
+
+    # epoch loss
+  
+if __name__ == '__main__':
+    main()
+
+    
---
+++ mlp.py
@@ -0,0 +1,46 @@
+# variational autoencoder
+
+import torch
+import torch.nn as nn
+
+class MLP(nn.Module):
+
+    def __init__(self):
+        super(MLP, self).__init__()
+        self.linear1 = nn.Linear(28, 80) # linear transformation 1
+        self.linear2 = nn.Linear(80, 80) # linear transformation 2
+        self.linear3 = nn.Linear(80, 2)   # linear transformation 3
+
+        self.activation = nn.ReLU() # activation function
+
+    def forward(self, data):
+
+    #    
+        x = self.linear1(data)
+        x = self.activation(x)
+        y = self.linear2(x)
+        y = self.activation(y)
+        z = self.linear3(y)
+      
+        return z
+
+class VAE(nn.Module):
+
+    def __init__(self):
+
+        super(VAE, self).__init__()
+
+        self.inputToLatent = nn.Linear(20, 10)
+        self.latentToOutput = nn.Linear(10, 20)
+
+    def forward(self, x):
+
+        # right now this is a simple autoencoder, without any priors, which is equivalent to principle component analysis.
+
+        z = x @ self.inputToLatent
+        out = z @ self.latentToOutput
+
+        return z
+
+
+
